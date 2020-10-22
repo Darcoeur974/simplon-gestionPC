a74c108d564b5c17e4089e89ad9928ac876e4db3
morgan <morgan@seyes.re>
2020-10-22 13:35:41 +0400

    config

--- public/assets/js/src/script.js
+++ public/assets/js/src/script.js
@@ -11,4 +11,30 @@ $(() => {
   //   dots: true
   // })
 
+    $('#computer-new').submit((e) => {
+        e.preventDefault()
+
+        const computerName = $('#computer_name').val()
+        let data = {
+            'name' : computerName
+        }
+
+        try {
+            $.ajax({
+                url: $('#computer-new').attr('action'),
+                method: 'POST',
+                data: data,
+                dataType: 'json',
+            })
+        } catch (e) {
+        }
+    })
+
+    $('.delete-computer').click((e) => {
+        e.preventDefault()
+
+        let cible = e.currentTarget.parentElement.parentElement.parentElement
+
+        cible.remove()
+    })
 })
