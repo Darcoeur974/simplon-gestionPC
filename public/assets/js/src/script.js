7cade4ca5abbe20cf9547f0d609c6b7c2e54f487
morgan <morgan@seyes.re>
2020-10-22 10:17:10 +0400

    Removed js and css files

---
+++ public/assets/js/src/script.js
@@ -0,0 +1,14 @@
+$(() => {
+
+  // Data Picker Initialization
+  // $('.datepicker').datepicker();
+  //
+  // /* owl-carousel */
+  // $('.owl-carousel').owlCarousel({
+  //   loop: true,
+  //   margin: 15,
+  //   nav: false,
+  //   dots: true
+  // })
+
+})
