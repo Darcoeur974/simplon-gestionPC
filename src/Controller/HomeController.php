34ca1809e26a98537418ba842280cfb00685f2df
morgan <morgan@seyes.re>
2020-10-22 10:17:38 +0400

    Added data computer in homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,43 @@
 
 namespace App\Controller;
 
+use App\Repository\ClientRepository;
+use App\Repository\ComputerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @param ComputerRepository $computerRepository
+     * @param ClientRepository $clientRepository
+     * @return Response
      */
-    public function index()
-    {
-        return $this->render('homepage.html.twig');
+    public function __invoke(
+        ComputerRepository $computerRepository, ClientRepository $clientRepository
+    ) {
+        $computers = $computerRepository->findAll();
+        $clients = $clientRepository->findAll();
+
+        $hours = [
+            '8h',
+            '9h',
+            '10h',
+            '11h',
+            '12h',
+            '13h',
+            '14h',
+            '15h',
+            '16h',
+            '17h',
+        ];
+
+        return $this->render('homepage.html.twig', [
+            'computers' => $computers,
+            'hours' => $hours,
+        ]);
     }
 }
